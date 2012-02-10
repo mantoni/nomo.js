7ec63568ffaec1c75ba9706d04d7e0d3ea9a703e
Maximilian Antoni <mail@maxantoni.de>
2012-02-10 22:28:40 +0100

    Configurable indentation for scripts.

--- test/test-script-indent.js
+++ test/test-script-indent.js
@@ -14,12 +14,20 @@ test('script.indent', {
     assert.strictEqual(s1, s2);
   },
 
-  'should indent given script by 2 spaces': function () {
+  'should indent script with 2 spaces by default': function () {
     var s = script('"foo";\n"bar";');
 
     s = s.indent();
 
     assert.equal(s.toString(), '  "foo";\n  "bar";');
+  },
+
+  'should indent script with given number of spaces': function () {
+    var s = script('"foo";\n"bar";');
+
+    s = s.indent(4);
+
+    assert.equal(s.toString(), '    "foo";\n    "bar";');
   }
 
 });
