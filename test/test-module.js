c8e7eb67356f10f16556ef9738fc6512d9cadaaf
Maximilian Antoni <mail@maxantoni.de>
2012-02-11 19:30:15 +0100

    Moved test cases.

--- test/test-module.js
+++ test/test-module.js
@@ -11,16 +11,40 @@ test('module', {
       module();
     }, 'TypeError');
   },
-  
+
   'should throw if file does not exist': function () {
     assert.throws(function () {
       module('some/unknown/path');
     }, 'Error');
   },
-  
+
   'should throw if directory has no index.js or package.json': function () {
     assert.throws(function () {
       module('test/fixture/lib');
+    }, 'Error');
+  },
+
+  'should set script to file': function () {
+    var m = module('test/fixture/a.js');
+
+    assert.equal('test/fixture/a.js', m.script);
+  },
+
+  'should set script to index.js': function () {
+    var m = module('test/fixture');
+
+    assert.equal('test/fixture/index.js', m.script);
+  },
+
+  'should set script to main file in package.json': function () {
+    var m = module('test/fixture/node_modules/c');
+
+    assert.equal('test/fixture/node_modules/c/c.js', m.script);
+  },
+
+  'should throw if main file in package.json does not exist': function () {
+    assert.throws(function () {
+      module('test/fixture/broken');
     }, 'Error');
   }
 
