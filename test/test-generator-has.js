688fbb23792bc5fb594ae8628301386a2804a38f
Maximilian Antoni <mail@maxantoni.de>
2012-02-12 14:50:11 +0100

    Added generator.has(...)

---
+++ test/test-generator-has.js
@@ -0,0 +1,22 @@
+var test      = require('utest');
+var assert    = require('assert');
+
+var generator = require('../lib/generator');
+
+
+test('generator.has', {
+
+  'should return ture if script was added': function () {
+    var g = generator();
+    g.add('foo', 'void 0;');
+    
+    assert(g.has('foo'));
+  },
+
+  'should return false if script was not added': function () {
+    var g = generator();
+    
+    assert(!g.has('foo'));
+  }
+
+});
